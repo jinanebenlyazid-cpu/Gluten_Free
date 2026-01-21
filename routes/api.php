31f76832ee29d6a557b65172e316c6944c2e0e89
Jinane <jinanebenliazid@gmail.com>
2026-01-21 11:19:05 +0100

    up route

--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@ use App\Http\Controllers\Api\ApiController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('Produits', ApiController::class);
+Route::apiResource('produits', ApiController::class);
 Route::get('/filter', [ApiController::class, 'filtrer']);
