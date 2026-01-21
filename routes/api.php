c7a7b50ca96134a07d4a612cc312eed6879d8d01
Jinane <jinanebenliazid@gmail.com>
2026-01-21 11:28:55 +0100

    route test

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@ use App\Http\Controllers\Api\ApiController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::apiResource('produits', ApiController::class);
-Route::get('/filter', [ApiController::class, 'filtrer']);
+//Route::apiResource('produits', ApiController::class);
+Route::get('/filter', [ApiController::class, 'index']);
 
 
 Route::get('/test', function () {
